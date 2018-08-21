9b6d7d2ddd0d70ae8255ce01622cb92efa128668
neckos <tech@necksystems.com>
2018-08-21 14:29:02 +0000

    latest

--- custom_manufacturing/custom_manufacturing/doctype/project_offer/project_offer.js
+++ custom_manufacturing/custom_manufacturing/doctype/project_offer/project_offer.js
@@ -59,6 +59,11 @@ frappe.ui.form.on('Project Offer', {
 		}
 	},
 	read: function(frm) {
+		function sleeper(ms) {
+		  return function(x) {
+		    return new Promise(resolve => setTimeout(() => resolve(x), ms));
+		  };
+		}
 		console.log("offer_description_template_name");
 		/*
 		var update_offer_description_details = function(doc, r) {
@@ -73,6 +78,7 @@ frappe.ui.form.on('Project Offer', {
 			refresh_field("offer_description_template_items");
 		}
 		*/
+		/*
 		return frappe.call({
 						type:"GET",
 						method: "custom_manufacturing.custom_manufacturing.doctype.project_offer.project_offer.get_offer_description_details",
@@ -85,7 +91,7 @@ frappe.ui.form.on('Project Offer', {
 							
 							if(r.message) {								
 								$.each(r.message, function(i, d) {
-									console.log(d.detail);
+									console.log(d);
 									var row = frappe.model.add_child(frm.doc, "Offer Description Detail", "project_offer_description_details");
 									row.category = d.category;
 									row.detail = d.detail;
@@ -97,15 +103,33 @@ frappe.ui.form.on('Project Offer', {
 								cur_frm.save()
 								
 							}
-							
-							
-							//frappe.model.clear_table(doc, "accounts");
-							//if(r.message) {
-							//	update_jv_details(doc, r.message);
-								//}
-							//cur_frm.set_value("is_opening", "Yes")
 						}
 					})
+		*/
+		new Promise(resolve => frappe.call({
+			type:"GET",
+			method: "custom_manufacturing.custom_manufacturing.doctype.project_offer.project_offer.get_offer_description_details",
+			args: {
+				"offer_description_template_name": frm.doc.offer_description_template_name
+			},
+			callback: resolve
+		})).then(r => {
+			frappe.model.clear_table(frm.doc, "project_offer_description_details");
+			if(r.message) {								
+				$.each(r.message, function(i, d) {
+					//console.log(d);
+					var row = frappe.model.add_child(frm.doc, "Offer Description Detail", "project_offer_description_details");
+					row.category = d.category;
+					row.detail = d.detail;
+					row.sub_detail = d.sub_detail;
+					row.type = d.type;
+					row.show = d.display;
+				});
+				frm.refresh_field("project_offer_description_details");	
+			}
+		
+		
+		}).then(sleeper(1000)).then(() => {cur_frm.save();console.log('after save');}).then(sleeper(1000)).then(() => {console.log('before scroll');frappe.utils.scroll_to(cur_frm.$wrapper.find("[data-fieldname='read']"), true);})
 
 	},
 	get_master_items_from_estimate: function(frm) {
