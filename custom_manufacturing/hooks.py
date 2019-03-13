6341e32c72e50a3a85866c5f34f2cff51478bbf7
neckos <tech@necksystems.com>
2019-03-13 18:29:04 +0000

    latest

--- custom_manufacturing/hooks.py
+++ custom_manufacturing/hooks.py
@@ -15,9 +15,11 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/custom_manufacturing/css/custom_manufacturing.css"
+
+app_include_css = "/assets/custom_manufacturing/css/custom_manufacturing.css"
 
 app_include_js = "/assets/js/custom_manufacturing.js"
+
 #app_include_js = "/assets/js/treeview_modul.js"
 
 # include js, css files in header of web template
